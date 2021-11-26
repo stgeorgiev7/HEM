53c3ccac9440a566e7c19d13f0e389bc04482da0
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-11-27 01:42:31 +0200

    feat: weather component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,15 +3,15 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
+import Weather from "../weather/Weather";
+import User from "../user/User";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
-          <Link href="/about" color="secondary">
-            Hi
-          </Link>
+            <Weather type={"rainy"} degrees={22}/>
         </Paper>
       </Container>
     </div>
---
+++ src/components/weather/Weather.jsx
@@ -0,0 +1,25 @@
+import { Typography, Stack } from "@mui/material";
+import classNames from "classnames";
+import styles from "./Weather.module.scss";
+
+export default function Weather({ degrees, type }) {
+
+    const icons = {
+        cloudy: "/images/cloudy.svg",
+        rainy: "/images/rainy.svg",
+        snowy: "/images/snowy.svg",
+        sunny: "/images/sunny.svg"
+    };
+
+    return (
+        <div className={classNames(styles["weather-wrapper"])}>
+            <img src={icons[type]} className={classNames(styles["weather-icon"])}></img>
+            <Stack>
+                <Typography className={classNames(styles["weather"])}>Weather</Typography>
+                <Typography sx={{ fontSize: "60px", fontWeight: 900, lineHeight:"70px" }}>{degrees} {'\u00b0'}</Typography>
+
+            </Stack>
+
+        </div>);
+
+}

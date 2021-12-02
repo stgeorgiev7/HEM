f6ed9fa719bd86e6ee11c3b30fe945c944db16f3
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-02 23:43:34 +0200

    fix: add onclick event

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,17 @@
 import styles from "./Card.module.scss"
 import classNames from "classnames"
-import { Card as CardContainer} from "@mui/material";
+import { Card as CardContainer } from "@mui/material";
 import { CardContent } from "@mui/material";
 
-export default function Card({iconUrl, outlined=false}) {
+export default function Card({ iconUrl, outlined = false, onClick }) {
 
     return (
         <div className={classNames(styles["card"])}>
             <CardContainer
-             sx={ outlined ? {border: "3px solid #7441F3"} : {border:'none'}}>
+                onClick={onClick}
+                sx={outlined ? { border: "3px solid #7441F3" } : { border: 'none' }}>
                 <CardContent>
-                    {iconUrl ? <img className={classNames(styles["card-image"])} src={iconUrl}></img> : <img></img> }
+                    <img className={classNames(styles["card-image"])} src={iconUrl ? iconUrl : ""}></img>
                 </CardContent>
             </CardContainer>
         </div>

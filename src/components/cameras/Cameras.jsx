e6b7a559df5b324b0dd8d4daa55834abc162a876
Emil Enev <emil.enev@abv.bg>
2021-12-05 19:40:17 +0200

    fix: fix autoplay and hide the plus button when false value is passed

--- src/components/cameras/Cameras.jsx
+++ src/components/cameras/Cameras.jsx
@@ -4,12 +4,12 @@ import { Grid } from "@mui/material";
 import Card from "../card/Card";
 import { useEffect, useState } from "react";
 
-export default function Cameras({ cameras, hasButton }) {
+export default function Cameras({ cameras, hasButton = false }) {
     const [video, setVideo] = useState('videos/bathroom.mp4') // default state should be the first video from cameras arr ??? 
-    const [outlined, setOutlined] = useState(false);
 
-    const getSrc = (event) => {
+    const changeVideo = (event) => {
         const src = event.target.getAttribute('src')
+        event.target.setAttribute("autoplay", true);
         setVideo(src)
     }
 
@@ -20,26 +20,28 @@ export default function Cameras({ cameras, hasButton }) {
                     <Card videoUrl={video}></Card>
                 </Grid>
                 <Grid container item xs={4} className={classNames(styles["items"], hasButton ? styles['scrolable'] : '')}>
-                    {cameras && cameras.map(el =>
+                    {/* {cameras && cameras.map(el =>
                     (<Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
                         <Card videoUrl={el.videoUrl}></Card>
-                    </Grid>))}
-
-                    {/* <Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
+                    </Grid>))} */}
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
                         <Card videoUrl={'videos/front-door.mp4'}></Card>
                     </Grid>
-                    <Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
+                        <Card videoUrl={'videos/front-door.mp4'}></Card>
+                    </Grid>
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
+                        <Card videoUrl={'videos/front-door.mp4'}></Card>
+                    </Grid>
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
                         <Card videoUrl={'videos/garden.mp4'}></Card>
                     </Grid>
-                    <Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
                         <Card videoUrl={'videos/kitchen.mp4'} ></Card>
                     </Grid>
-                    <Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
                         <Card videoUrl={'videos/balcony.mp4'} ></Card>
                     </Grid>
-                    <Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
-                        <Card videoUrl={'videos/garden.mp4'} ></Card>
-                    </Grid> */}
                     {hasButton &&
                         <Grid item className={classNames(styles["btn"])}>
                             <Card iconUrl={'images/plus.svg'} outlined></Card>

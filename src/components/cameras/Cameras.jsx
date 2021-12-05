dbbc7f8d299356716432e01fc70cff19a00b9cc2
Emil Enev <emil.enev@abv.bg>
2021-12-05 20:52:47 +0200

    fix: fix autoplay video

--- src/components/cameras/Cameras.jsx
+++ src/components/cameras/Cameras.jsx
@@ -5,26 +5,27 @@ import Card from "../card/Card";
 import { useEffect, useState } from "react";
 
 export default function Cameras({ cameras, hasButton = false }) {
-    const [video, setVideo] = useState('videos/bathroom.mp4') // default state should be the first video from cameras arr ??? 
+    const [videoUrl, setVideoUrl] = useState('videos/bathroom.mp4') // default state should be the first video from cameras arr ??? 
 
     const changeVideo = (event) => {
         const src = event.target.getAttribute('src')
-        event.target.setAttribute("autoplay", true);
-        setVideo(src)
+        setVideoUrl(src)
     }
+
+    useEffect(() => {
+        const video = document.querySelector('.video-player video')
+        video.autoplay = true;
+        video.load();
+    }, [videoUrl])
 
     return (
         <div className={classNames(styles["container"])}>
             <Grid container className={classNames(styles["cameras-container"])}>
-                <Grid item xs={8} className={classNames(styles["video-player"])}>
-                    <Card videoUrl={video}></Card>
+                <Grid item xs={8} className={classNames("video-player")}>
+                    <Card videoUrl={videoUrl}></Card>
                 </Grid>
                 <Grid container item xs={4} className={classNames(styles["items"], hasButton ? styles['scrolable'] : '')}>
-                    {cameras && cameras.map(el =>
-                    (<Grid item xs={5} onClick={getSrc} className={classNames(styles["video-item"])}>
-                        <Card videoUrl={el.videoUrl}></Card>
-                    </Grid>))}
-                    {/* <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
+                    <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
                         <Card videoUrl={'videos/front-door.mp4'}></Card>
                     </Grid>
                     <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
@@ -41,7 +42,7 @@ export default function Cameras({ cameras, hasButton = false }) {
                     </Grid>
                     <Grid item xs={5} onClick={changeVideo} className={classNames(styles["video-item"])}>
                         <Card videoUrl={'videos/balcony.mp4'} ></Card>
-                    </Grid> */}
+                    </Grid>
                     {hasButton &&
                         <Grid item className={classNames(styles["btn"])}>
                             <Card iconUrl={'images/plus.svg'} outlined></Card>

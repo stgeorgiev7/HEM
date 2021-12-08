56997deade23cca6a2bee21683eea525197f219a
Emil Enev <emil.enev@abv.bg>
2021-12-08 23:04:46 +0200

    feat: implement Device component

--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -1,26 +1,28 @@
 import styles from "./Devices.module.scss"
 import classNames from "classnames"
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import Card from "../card/Card";
 
 export default function Devices({ devices }) {
     return (
-        <Grid container spacing={3} maxWidth="850px">
-            <Grid item >
-                <Card iconUrl={"images/rock.svg"} title="add room"></Card>
+        <div className={classNames(styles["devices-wrapper"])}>
+            <Grid container spacing={3} >
+                <Grid item >
+                    <Card iconUrl={"images/bulb.svg"} variant="on"></Card>
+                </Grid>
+                <Grid item >
+                    <Card iconUrl={"images/bulb.svg"} variant="off"></Card>
+                </Grid>
+                <Grid item >
+                    <Card iconUrl={"images/plug.svg"} variant="off"></Card>
+                </Grid>
+                <Grid item>
+                    <Card iconUrl={"images/plug.svg"} variant="offline"></Card>
+                </Grid>
+                <Grid item>
+                    <Card iconUrl={"images/plus.svg"}></Card>
+                </Grid>
             </Grid>
-            <Grid item >
-                <Card iconUrl={"images/rock.svg"} title="add room"></Card>
-            </Grid>
-            <Grid item >
-                <Card iconUrl={"images/plus.svg"} outlined></Card>
-            </Grid>
-            <Grid item>
-                <Card iconUrl={"images/plus.svg"} outlined></Card>
-            </Grid>
-            <Grid item>
-                <Card iconUrl={"images/plus.svg"} outlined></Card>
-            </Grid>
-        </Grid>
+        </div>
     )
 }

8c311539689e0f173622756b23fb5b8b8a7fe2bf
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-15 21:39:06 +0200

    fix: changed addScene path

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Modal from "../src/components/modal/Modal";
 import Dashboard from "../src/components/dashboard/Dashboard";
 import SceneComposer from "../src/components/scenes/SceneComposer";
-import AddScene from "../src/components/addScene/AddScene"
+import AddScene from "../src/components/scenes/AddScene"
 import roomsData from "../data/rooms.json"
 import devicesData from "../data/devices.json"
 

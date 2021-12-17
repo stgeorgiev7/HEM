f097f88dcc571d80c414d854df25bf48e09e42f5
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-17 16:42:42 +0200

    fix: add outlined to card

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,20 @@ import devicesData from "../data/devices.json"
 export default function Index() {
   return (
     <div>
-      <EditScene />
+      <EditScene
+      open
+      handleClose={() => {alert('close')}} 
+      devices={devicesData?.devices}
+      rooms={roomsData?.rooms}
+      selected={
+        {
+          "name": "Living Room",
+          "type": "living-room",
+          "id": 1,
+          "propertyId": 1,
+          "userId": 1
+        }
+      }/>
     </div>
   );
 }

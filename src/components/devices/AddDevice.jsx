1d2c499176adc00eb3c04ef85a2ca69fd080c38d
Emil Enev <emil.enev@abv.bg>
2021-12-22 22:25:39 +0200

    feat: add AddDevice component

---
+++ src/components/devices/AddDevice.jsx
@@ -0,0 +1,11 @@
+import styles from "./AddDevice.module.scss"
+import classNames from "classnames"
+import Modal from "../modal/Modal"
+
+export default function AddDevice({ searching, found, failed, onDevice }) {
+    return (
+        <Modal>
+
+        </Modal>
+    )
+}

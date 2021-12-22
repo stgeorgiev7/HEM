78fd0efc4016937cf2c1ed62e615e7e7bdd0615c
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-22 22:28:28 +0200

    fix: modal title

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Dialog, DialogTitle, DialogContent, Button } from "@mui/material";
 import classNames from "classnames";
 
-export default function Modal({ open, handleSubmit, handleClose, children, buttonProps }) {
+export default function Modal({ open, handleSubmit, handleClose, children, title, buttonProps }) {
     const [openDialog, setOpenDialog] = useState(open);
 
     handleClose = () => {
@@ -14,7 +14,7 @@ export default function Modal({ open, handleSubmit, handleClose, children, butto
     return (
         <div >
             <Dialog open={open} onClose={handleClose} className={classNames(styles["modal-container"])}>
-                <DialogTitle className={classNames(styles.title)}>{buttonProps?.title}</DialogTitle>
+                <DialogTitle className={classNames(styles.title)}>{title}</DialogTitle>
                 <DialogContent className={classNames(styles.content)}>
                     {children}
                 </DialogContent>

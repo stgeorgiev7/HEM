7a2b9ba2d9e46fe37aeea1f6c419e0a11339857c
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-22 20:26:05 +0200

    fix: added color to buttonProps

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Dialog, DialogTitle, DialogContent, Button } from "@mui/material";
 import classNames from "classnames";
 
-export default function Modal({ open, title, buttonText, handleSubmit, handleClose, children, buttonProps }) {
+export default function Modal({ open, handleSubmit, handleClose, children, buttonProps }) {
     const [openDialog, setOpenDialog] = useState(open);
 
     handleClose = () => {
@@ -14,11 +14,15 @@ export default function Modal({ open, title, buttonText, handleSubmit, handleClo
     return (
         <div >
             <Dialog open={open} onClose={handleClose} className={classNames(styles["modal-container"])}>
-                <DialogTitle className={classNames(styles.title)}>{title}</DialogTitle>
+                <DialogTitle className={classNames(styles.title)}>{buttonProps?.title}</DialogTitle>
                 <DialogContent className={classNames(styles.content)}>
                     {children}
                 </DialogContent>
-                <Button onClick={handleSubmit}>{buttonText}</Button>
+                <Button
+                 onClick={handleSubmit}
+                 color={buttonProps?.color}
+                 >{buttonProps?.buttonText}
+                 </Button>
             </Dialog>
         </div>
     )

c5bcfc35977a6af260ce2d658fac6a00fed07d9b
Emil Enev <emil.enev@abv.bg>
2021-12-23 01:47:00 +0200

    fix: fix

--- src/components/devices/AddDevice.jsx
+++ src/components/devices/AddDevice.jsx
@@ -35,7 +35,7 @@ export default function AddDevice({ open, handleClose, handleSubmmit, searching 
                     <img src={searching ? "images/search.svg" : "images/no-connection.svg"} ></img>
                 </Box>
             </Box>
-            <Typography sx={{ color: "#7441F3", fontWeight: "700", textAlign: "center", fontWeight: "700", marginTop: "45px" }}>{searching ? "Searching..." : failed ? "No Device Found" : ""}</Typography>
+            <Typography sx={{ color: "#7441F3", fontWeight: "700", textAlign: "center", fontWeight: "700", marginTop: "45px" }}>{failed ? "No Device Found" : ""}</Typography>
         </Modal>
     )
 }

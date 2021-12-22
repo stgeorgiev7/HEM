6a38b16a80f0c1c7ab96bb2749ed1a74dc3c0ab5
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-22 08:09:28 +0200

    feat: add changeRoom component

---
+++ src/components/rooms/ChangeRoom.jsx
@@ -0,0 +1,36 @@
+import styles from "./ChangeRoom.module.scss";
+import classNames from "classnames";
+import { Grid } from "@mui/material";
+import Modal from "../modal/Modal";
+import Card from "../card/Card";
+
+export default function ChangeRoom({ open, handleClose, rooms = [], onSubmit }) {
+
+    return (
+        <Modal
+            open={open}
+            title="CHANGE ROOM"
+            buttonText="SAVE CHANGES"
+            handleClose={handleClose}
+            onSubmit={onSubmit}>
+            <Grid container
+             spacing={3}
+             justifyContent="center">
+                {rooms?.map((crrRoom, index) => {
+                    return (
+                        <Grid item key={index}>
+                            <Card
+                                iconUrl={"images/bed.svg"}
+                                title={crrRoom?.name}
+                                variant="offline"
+                            />
+                        </Grid>
+                    )
+                })}
+
+            </Grid>
+
+        </Modal>
+
+    )
+}

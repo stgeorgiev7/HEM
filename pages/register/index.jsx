a7be29cb56e84d13ace0afb6c4969ac770b9d7ad
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-12-25 16:36:47 +0200

    fix:  added fetch to index

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react"
 import styles from "./RegisterPage.module.scss";
 import classNames from "classnames";
 import { Paper, Container, Typography } from "@mui/material";
@@ -5,13 +6,41 @@ import Register from "../../src/components/register/Register";
 import Link from "../../src/components/link/Link";
 
 export default function RegisterPage() {
+    const [userName, setUser] = useState();
+    const [password, setPassword] = useState();
+    const [retypedpass, setRetype] = useState();
+
+    async function registerUser() {
+        console.log("kur");
+        if (password === retypedpass) {
+            await fetch("https://hem-api.herokuapp.com/register", {
+                method: "POST",
+                body: JSON.stringify({
+                    email: `${userName}`,
+                    password: `${password}`,
+                }),
+            }).then((response) => {
+                if (response.status === 200) {
+                    alert(`Success ! ${userName} Registered!`);
+                }
+            });
+        } else {
+            alert(`Password is not matching!`);
+        }
+    };
+
     return (
         <div className={classNames(styles["wrapper"])}>
             <Container
                 className={classNames(styles["container"])}
                 maxWidth="xl">
                 <Paper className={classNames(styles["paper"])}>
-                    <Register />
+                    <Register
+                        onSubmit={registerUser}
+                        setUser={setUser}
+                        setPassword={setPassword}
+                        setRetype={setRetype}
+                    />
                 </Paper>
                 <Typography className={classNames(styles["text"])}>
                     Already registered? <Link href="/login">Go to login.</Link>
